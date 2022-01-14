93af96fa5d2ded06055f5afd7e6b1edf94d4a545
JakobJohannesson <JakobJohannesson96@gmail.com>
2022-01-15 00:18:20 +0100

    This is the message

--- R/borsdata.R
+++ R/borsdata.R
@@ -12,8 +12,8 @@ fetch_branches<-function(key=key){
   getdata<-httr::GET(url=paste0("https://apiservice.borsdata.se/v1/branches?authKey=", key))
   data_json <- httr::content(getdata, type="text", encoding = "UTF-8")
   df <- jsonlite::fromJSON(data_json,)
-  df<-df$branches
-  return(df)
+  df2<-df$branches
+  return(df2)
 }
 
 #' Fetch Countries
